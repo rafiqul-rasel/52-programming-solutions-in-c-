2afb9ef2ed9258e3503c3f3f180488ec7aefa766
Md.Rafiqul Islam <mrafiqulislam@duet.ac.bd>
2020-06-15 12:46:09 +0600

    1 st problem solved

--- 1.odd or even 1/main.cpp
+++ 1.odd or even 1/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    int T,i,n;
+    cin>>T;
+    for(i=1;i<=T;i++){
+        cin>>n;
+        if(n%2==0){
+            cout<<"even\n";
+        }else{
+        cout<<"odd\n";
+        }
+    }
     return 0;
 }
